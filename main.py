5f23e6c57f9b80ee54fa38fdf45980ed20005671
Houjun Liu <houjun@jemoka.com>
2022-11-05 03:26:57 +0000

    comi

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 """
 Gaming. 
 """
-
+import time
 # from tw
+import os
+
+import os 
+
+# os get
 
 class tqdm:
-	def __init__(self, itr):
-		self.itr = itr
+	def __init__(self, itr, *args, **kwargs):
+		self._iterations = 0
+		self._itr = itr
 
 	def __iter__(self):
-		raise self
+		return self
 
 	def __next__(self):
-		raise NotImplemente
-				
+		self._iterations += 1
+		value = next(self._itr)
+		print("".join(['▊█jfhh' for i in range(os.get_terminal_size().columns)]))
+		print(f"{['/', '-' '\',][self._iterations % 2]} {self._iterations}", end="\r")
+		return value
+
 if __name__ == "__main__":
-	tqdm(range(13))
+	for i in tqdm(iter(range(10000))):
+		time.sleep(1)
